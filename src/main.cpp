15b3bcc81eeb56a63f0acf84cdecb4556a88e168
QT <lquang4321@gmail.com>
2021-03-13 02:01:20 -0500

    Full GUI added

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,24 @@
-/*-----03/01/2021----*/
-/*-----CompactPCR----*/
-#include <Arduino.h>            // If you are using Arduino IDE, you can remove or ignore this line.
-#include <Adafruit_AS7341.h>    // Color sensor library
-#include <Plotter.h>            // Displays 2D Graph of X vs Y
-#include <Wire.h>               // I2C library
-#include <Adafruit_GFX.h>       // Graphic library
-#include <Adafruit_SSD1306.h>   // OLED Library
-#include <RotaryEncoder.h>      // Encoder 
-#include "GUI.h"                // Needs a complete rewrite
+/*------03/13/2021------*/
+#include <Adafruit_I2CDevice.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include <RotaryEncoder.h>
+#include <menu.h>
+#include <menuIO/serialOut.h>
+#include <menuIO/serialIn.h>
+#include <menuIO/chainStream.h>
+#include <menuIO/rotaryEventIn.h>
+#include <menuIO/adafruitGfxOut.h>
+#include <Adafruit_SSD1306.h>
+#include <Plotter.h>
+
+#include <AceButton.h> // https://github.com/bxparks/AceButton
+
+using namespace Menu;
+using namespace ::ace_button;
+
+
+
 
 /*--------READ BAT VOLTAGE---------*/
 #define VBATPIN A7
@@ -15,28 +26,56 @@
 #define VBAT_DIVIDER      (0.71275837F)   // 2M + 0.806M voltage divider on VBAT = (2M / (0.806M + 2M))
 #define VBAT_DIVIDER_COMP (1.403F)        // Compensation factor for the VBAT divider
 #define REAL_VBAT_MV_PER_LSB (VBAT_DIVIDER_COMP * VBAT_MV_PER_LSB)
+float battVOLT;
 /*--------READ BAT VOLTAGE---------*/
 
-
 /*--------COLOR SENSOR---------*/
+#include <Adafruit_AS7341.h>
 Adafruit_AS7341 as7341;
 /*--------COLOR SENSOR---------*/
 
 /*--------ENCODER---------*/
-#define ENC_A     15
-#define ENC_B     11
-int counter = 0;
+#define ROTARY_PIN_BUT 27
+AceButton button(ROTARY_PIN_BUT, /*default is pull-up high*/HIGH);             
+
+RotaryEventIn reIn(
+  RotaryEventIn::EventType::BUTTON_CLICKED | // select
+  RotaryEventIn::EventType::BUTTON_DOUBLE_CLICKED | // back
+  RotaryEventIn::EventType::BUTTON_LONG_PRESSED | // also back
+  RotaryEventIn::EventType::ROTARY_CCW | // up
+  RotaryEventIn::EventType::ROTARY_CW // down
+); // register capabilities, see AndroidMenu MenuIO/RotaryEventIn.h file
+
+void encoder( int8_t step)                                                                      //Encoder rotation 
+{
+    if( step)
+    {
+        if( step > 0 /* 1 == CW, -1 == CCW, 0 == idle*/ )
+        {
+            reIn.registerEvent(RotaryEventIn::EventType::ROTARY_CW);
+        } else
+        {
+            reIn.registerEvent(RotaryEventIn::EventType::ROTARY_CCW);
+        }
+    }
+}
+
+void handleButtonEvent(AceButton* /* button */, uint8_t eventType, uint8_t buttonState) {       //Handles button (click, double click, click&hold)
+
+  switch (eventType) {
+    case AceButton::kEventClicked:
+      reIn.registerEvent(RotaryEventIn::EventType::BUTTON_CLICKED);
+      break;
+    case AceButton::kEventDoubleClicked:
+      reIn.registerEvent(RotaryEventIn::EventType::BUTTON_DOUBLE_CLICKED);
+      break;
+    case AceButton::kEventLongPressed:
+      reIn.registerEvent(RotaryEventIn::EventType::BUTTON_LONG_PRESSED);
+      break;
+  }
+}
+
 /*--------ENCODER---------*/
-
-
-/*--------OLED STUFF---------*/
-//U8X8_SSD1306_128X64_VCOMH0_HW_I2C display(/* reset=*/ U8X8_PIN_NONE);  
-/*--------OLED STUFF---------*/
-
-/*--------User Interface---------*/
-int pageNum;
-int cursorPos = 12;
-/*--------User Interface---------*/
 
 /*--------THERMISTOR---------*/
 //https://learn.adafruit.com/thermistor/using-a-thermistor
@@ -75,6 +114,23 @@ bool inAuto = false;
 #define AUTOMATIC 1
 /*--------PID---------*/
 
+/*--------DISPLAY---------*/
+#define fontX 6
+#define fontY 9
+#define WIDTH 128
+#define HEIGHT 64
+
+Adafruit_SSD1306 display(WIDTH,HEIGHT);
+const colorDef<uint16_t> colors[6] MEMMODE={
+  {{BLACK,WHITE},{BLACK,WHITE,WHITE}},//bgColor
+  {{WHITE,BLACK},{WHITE,BLACK,BLACK}},//fgColor
+  {{WHITE,BLACK},{WHITE,BLACK,BLACK}},//valColor
+  {{WHITE,BLACK},{WHITE,BLACK,BLACK}},//unitColor
+  {{WHITE,BLACK},{BLACK,BLACK,BLACK}},//cursorColor
+  {{WHITE,BLACK},{BLACK,WHITE,WHITE}},//titleColor
+};
+/*--------DISPLAY---------*/
+
 /*--------Plotter---------*/
 double w, x, z, F8_680;
 int y;
@@ -82,16 +138,33 @@ Plotter p;
 /*--------Plotter---------*/
 
 /*--------Millis Delay------*/
-const long eventTime_1 = 1;  //in ms
-const long eventTime_2 = 25;  //in ms
-const long eventTime_3 = 25;  //in ms
-const long eventTime_4 = 25;  //in ms
+const long eventTime_1 = 200;  //in ms
+const long eventTime_2 = 200;  //in ms
+const long eventTime_3 = 200;  //in ms
+const long eventTime_4 = 200;  //in ms
 
 unsigned long previousTime_1 = 0;
 unsigned long previousTime_2 = 0;
 unsigned long previousTime_3 = 0;
 unsigned long previousTime_4 = 0;
 /*--------Millis Delay------*/
+
+/*--------MISC---------*/
+uint16_t    denatureTemp = 25,
+            annealTemp = 25,
+            extendTemp = 25;
+
+uint16_t    cycle = 5;
+
+uint16_t    timeOn = 100;
+uint16_t    timeOff = 100;
+
+int ledCtrl=LOW;
+bool blink(int timeOn,int timeOff) 
+{
+    return millis()%(unsigned long)(timeOn+timeOff)<(unsigned long)timeOn;
+}
+/*--------MISC---------*/
 
 void Compute()
 {
@@ -219,8 +292,79 @@ float readBAT(int pin){
 
 
 
-void setup(void) {
-    Serial.begin(115200);
+
+/*--------GUI---------*/
+result myLedOn() {
+    ledCtrl=HIGH;
+    return proceed;
+}
+result myLedOff() {
+    ledCtrl=LOW;
+    return proceed;
+}
+result ledToggle(){
+    ledCtrl = !ledCtrl;
+    return proceed;
+}
+
+MENU(checkSensors, "SENSORS READ",doNothing,noEvent,wrapStyle                  //Working
+    ,FIELD(y,"Current Temp: ","C",25,150,0,0,doNothing, noEvent, noStyle) 
+    ,FIELD(battVOLT,"Batt Voltage: ","V",2,5,0,0,doNothing, noEvent, noStyle) 
+    ,FIELD(w,"PID Output: ","%",0,100,0,0,doNothing, noEvent, noStyle) 
+    ,EXIT("<Back")
+);
+
+MENU(setConfig, "SETUP",doNothing,noEvent,wrapStyle                  //Working
+    ,FIELD(denatureTemp,"Denature: ","c",25,100,1,0,Menu::doNothing,Menu::noEvent,Menu::noStyle)
+    ,FIELD(annealTemp,"Anneal: ","c",25,100,1,0,Menu::doNothing,Menu::noEvent,Menu::noStyle)
+    ,FIELD(extendTemp,"Extension: ","c",25,100,1,0,Menu::doNothing,Menu::noEvent,Menu::noStyle)
+    ,FIELD(cycle,"Cycle: ","",0,100,1,0,Menu::doNothing,Menu::noEvent,Menu::noStyle)
+    ,EXIT("<Back")
+);
+
+MENU(about, "ABOUT",doNothing,noEvent,wrapStyle                         //Works
+    ,OP("CCNYSenior II Spr'21", doNothing, noEvent)                             //Works
+    ,OP("      AUTHOR        ", doNothing, noEvent)                                  //Works  
+    ,OP("Quang T, Yossel N,", doNothing, noEvent)                                   //Works
+    ,OP("Gnimdou T, Yousra T", doNothing, noEvent)                                  //Works    
+    ,OP("      MENTOR        ", doNothing, noEvent)                                  //Works 
+    ,OP("Prof. Sang-woo Seo", doNothing, noEvent)                           //Works
+    ,EXIT("<Back")
+);
+
+MENU(mainMenu,"MAIN",doNothing,noEvent,wrapStyle
+    ,OP("Run", ledToggle, enterEvent)                                                //Works
+    ,FIELD(timeOn,"OnTime: ","ms",1,1000,10,1, doNothing, noEvent, noStyle)         //Works
+    ,FIELD(timeOff,"OffTime: ","ms",1,1000,10,1,doNothing, noEvent, noStyle)        //Works
+    ,SUBMENU(setConfig)
+    ,SUBMENU(about)
+    ,SUBMENU(checkSensors)
+    // ,OP("LED On",myLedOn,enterEvent)
+    // ,OP("LED Off",myLedOff,enterEvent)
+    ,EXIT("<Back")
+);
+
+#define MAX_DEPTH 2
+
+MENU_OUTPUTS(out,MAX_DEPTH
+    ,ADAGFX_OUT(display,colors,fontX,fontY,{0,0,WIDTH/fontX,HEIGHT/fontY})
+    ,SERIAL_OUT(Serial)
+    ,NONE
+);
+
+serialIn serial(Serial);        //Serial input
+MENU_INPUTS(in,&reIn);          //Physical input
+
+NAVROOT(nav,mainMenu,MAX_DEPTH,in,out);
+
+/*--------GUI---------*/
+
+
+
+
+void setup(void)
+{
+
     Wire.begin();
     //analogReference(3.43);
     SetOutputLimits(0, 255);
@@ -236,117 +380,70 @@ void setup(void) {
     as7341.setGain(AS7341_GAIN_128X);
     as7341.startReading();
     as7341.enableLED(false);
+    pinMode(LED_BUILTIN, OUTPUT);
 
-    display.begin(SSD1306_EXTERNALVCC, 0x3C);
+    pinMode(ROTARY_PIN_BUT, INPUT_PULLUP);
+    ButtonConfig* buttonConfig = button.getButtonConfig();
+    buttonConfig->setEventHandler(handleButtonEvent);
+    buttonConfig->setFeature(ButtonConfig::kFeatureClick);
+    buttonConfig->setFeature(ButtonConfig::kFeatureDoubleClick);
+    buttonConfig->setFeature(ButtonConfig::kFeatureLongPress);
+    buttonConfig->setFeature(ButtonConfig::kFeatureSuppressClickBeforeDoubleClick);
+    buttonConfig->setFeature(ButtonConfig::kFeatureSuppressAfterClick);
+    buttonConfig->setFeature(ButtonConfig::kFeatureSuppressAfterDoubleClick);
+    
+    RotaryEncoder.begin(7, 30);
+    RotaryEncoder.setDebounce(true);
+    //RotaryEncoder.setSampler(5120);
+    RotaryEncoder.start();
+
+    display.begin(SSD1306_EXTERNALVCC, 0x3c);
     display.clearDisplay();
-    display.setTextSize(2); /* Select font size of text. Increases with size of argument. */
-    display.setTextColor(WHITE); /* Color of text*/
-    display.setCursor(0,0);
-    display.print("CompactPCR");
-    display.setTextSize(1);
-    display.setCursor(24,20);
-    display.print("EE598 Spr'21");
-    display.setCursor(0,32);
-    display.setTextSize(1);
-    display.print("By: Quang T,Yossel N,   Yousra T,Gnimdou T");
+    delay(100);
     display.display();
-    display.setTextSize(1);
-    delay(5000);
+    // u8x8.begin();
+    // u8x8.setFont(fontName);
+    // u8x8.drawString(0,0,"Hello");
+    // u8x8.print("Menu 4.x");
 
-    //display.begin();
-    //display.setFont(u8x8_font_5x7_f);
-
+    //Serial.begin(115200);
     // Start plotter
     p.Begin();
 
     // Add 5 variable time graph
     p.AddTimeGraph( "PID", 800, "PID Output %", w, "Setpoint", x, "Temp", y, "Error", z, "F8_680 RED", F8_680 );
-
-    RotaryEncoder.begin(ENC_A, ENC_B);  // Initialize Encoder
-    RotaryEncoder.start();              // Start encoder
 }
 
-
-void loop(void) {
+void loop(void)
+{
     unsigned long currentTime = millis();
-
-    int value = RotaryEncoder.read();
-
-    if (value)
-    {
-        if ( value > 0 )
-        {
-            if( cursorPos < 44){
-                cursorPos += 8;
-            }
-            counter++;
-        }else
-        {
-            if( cursorPos >= 20){
-                cursorPos -= 8;
-            }
-            counter--;
-        }
-    }    
-
+    battVOLT = readBAT(VBATPIN);
     if ( currentTime - previousTime_1 >= eventTime_1) {
         as7341.readAllChannels( );
         F8_680 = as7341.getChannel(AS7341_CHANNEL_680nm_F8);
 
         previousTime_1 = currentTime;
     }
-
-    //Serial.print("TargetTemp:");
-    //Serial.print(Setpoint);
-    //Serial.print(" Temp:");
-    //Serial.print(getTemp());
     Setpoint = map(analogRead(A4), 0, 1023, 25, 80);
     Input = getTemp();
-    //Serial.print(" Error:");
-    //Serial.print(Setpoint - Input);
-    Compute();
-
-    //Serial.print(" PID_Output:");
-    //Serial.println(Output);
     w = map(Output, 0, 255, 0, 100);
     x = Setpoint;
     y = getTemp();
     z = Setpoint - Input;
-
-    if ( currentTime - previousTime_2 >= eventTime_2) {
-        display.clearDisplay();
-        display.setTextSize(1);
-        display.setCursor(0, 0);
-        display.drawLine(0,10,127,10, WHITE);
-        display.fillRect(0, 0, 75, 10, BLACK);
-        display.println(" HOME");
-        display.setCursor(64,0);
-        display.printf("BATT:%-.2fV", readBAT(VBATPIN));
-
-        display.setCursor(0, cursorPos);
-        display.print(">");
-
-        display.setCursor(8,12);
-        display.printf("SetTmp: %-.0f Cel", x);
-
-        display.setCursor(8,20);
-        display.printf("CurTmp: %-02d Cel", y);
-        
-        display.setCursor(8,28);
-        display.printf("Encder: %-02d", counter);
-
-        display.setCursor(8,36);
-        display.printf("PIDout: %-.0f%%", w);
-
-        display.setCursor(8,44);
-        display.printf("680_nM: %-.1f", F8_680);
-
-        display.display();
-
-        previousTime_2 = currentTime;
-    }
+    Compute();
     p.Plot(); // usually called within loop()
-
     analogWrite(HeaterPIN, Output);
 
+    encoder( RotaryEncoder.read() );
+    button.check();
+    digitalWrite(LED_BUILTIN, blink(timeOn, timeOff));
+
+    //nav.poll();              //Polling based, laggy inputs but better for time sensitive application
+    //display.display();    
+
+    nav.doInput();              //Event Based. This display method is more responsive, but lags background process
+    if (nav.changed(0)) {       //only draw if changed
+        nav.doOutput();
+        display.display();
+    }
 }
